cbcdb7460c944f3da20f099af4ae5f809c084510
RISHI <Rishi rishi.h2kinfosys@gmail.com>
2019-12-03 22:22:18 -0500

    Commit JDBC Session 1

---
+++ src/com/learn/jdbc/FetchFilms.java
@@ -0,0 +1,11 @@
+package com.learn.jdbc;
+
+public class FetchFilms {
+
+	public static void main(String[] args) {
+		// Fetch and Print First 6 columns of Film table
+		
+
+	}
+
+}
---
+++ src/com/learn/jdbc/TestJDBCConnection.java
@@ -0,0 +1,59 @@
+package com.learn.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.Properties;
+
+import com.mysql.cj.jdbc.Driver;
+
+public class TestJDBCConnection {
+
+	private static String url =  "jdbc:mysql://localhost:3306/sakila";
+	private static String userName = "root";
+	private static String password = "password";
+	private static String selectAllActors = "Select * from Actor";
+	
+	
+	public static void main(String[] args) {
+		
+		try {
+			// Step 1. Driver
+			Driver mysqlDriver = new Driver();
+			// Step 2 DriverManager
+			DriverManager.registerDriver(mysqlDriver);
+			// Step 3 - Connection
+			// URL (IP/ port/ name / scema ) - UserName - Password
+			Properties props = new Properties();
+			props.setProperty("user", userName);
+			props.setProperty("password", password);
+			Connection conn = DriverManager.getConnection(url, props);
+			// Step 4: Statement
+			Statement stat = conn.createStatement();
+			// Step 5: Get the results
+			ResultSet rs = stat.executeQuery(selectAllActors);
+			if(rs != null) {
+				String firstName = null;
+				String lastname = null;
+				Timestamp timestamp = null;
+				int actorId = 0;
+				while(rs.next()) {
+					actorId = rs.getInt("actor_id");
+					firstName = rs.getString("first_name");
+					lastname = rs.getString("last_name");
+					timestamp = rs.getTimestamp("last_update");
+					System.out.println(actorId + "  " + firstName + "  " + lastname + "  " + timestamp);
+				}
+			}
+			conn.close();
+			// Step 6 - in case of issues - SQLException
+		}catch(SQLException sqlEx) {
+			sqlEx.printStackTrace();
+		}
+
+	}
+
+}
